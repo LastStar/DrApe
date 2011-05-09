57c325dd1b9ae34d0239d8df63b599eb2cb97a38
Štefan Ľupták <stefan.luptak@laststar.eu>
2011-05-09 11:52:25 +0200

    Min tiles 4, max tiles 8

--- BeatTheMonkey/Utils.h
+++ BeatTheMonkey/Utils.h
@@ -13,8 +13,8 @@
 #define BTM_DEBUG 1
 #define GAME_MODE 0
 #define TILES_COUNT 3
-#define TILES_COUNT_MIN 3
-#define TILES_COUNT_MAX 7
+#define TILES_COUNT_MIN 4
+#define TILES_COUNT_MAX 8
 #define TILES_X 8
 #define TILES_Y 5
 #define DIFFICULTY 0
